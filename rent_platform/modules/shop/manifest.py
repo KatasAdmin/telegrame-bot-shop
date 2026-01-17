8c491d7ffea6df55f1c5c90e4717a9fc1ffeaebe
KatasAdmin <katasonoviktor@gmail.com>
2026-01-17 21:28:47 +0200

    Update manifest.py

--- rent_platform/modules/shop/manifest.py
+++ rent_platform/modules/shop/manifest.py
@@ -1,1 +1,10 @@
-#
+# rent_platform/modules/shop/manifest.py
+
+MANIFEST = {
+    "name": "shop",
+    "title": "🛒 Магазин",
+    "description": "Повноцінний Telegram-магазин з товарами та замовленнями",
+    "version": "1.0.0",
+    "price_month": 100,  # грн / місяць
+    "entry": "rent_platform.modules.shop.router:handle_update",
+}
