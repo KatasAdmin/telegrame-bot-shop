788e4662e7f0a02000688449c2e0cb9e50e9c005
KatasAdmin <katasonoviktor@gmail.com>
2026-01-17 21:31:02 +0200

    Update registry.py

--- rent_platform/core/registry.py
+++ rent_platform/core/registry.py
@@ -11,3 +11,20 @@ def register_module(name: str, router_factory: Callable):
 
 def get_module(name: str):
     return MODULES.get(name)
+
+# rent_platform/core/registry.py
+
+from rent_platform.modules.shop.router import handle_update as shop_handler
+
+MODULES = {}
+
+def register_module(name: str, router_factory):
+    MODULES[name] = router_factory
+
+
+def get_module(name: str):
+    return MODULES.get(name)
+
+
+# 🔥 РЕЄСТРАЦІЯ МОДУЛІВ
+register_module("shop", shop_handler)
