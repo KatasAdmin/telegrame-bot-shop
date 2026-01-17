5a814a72430227364b56ba771069e887f57358f6
KatasAdmin <katasonoviktor@gmail.com>
2026-01-17 21:25:48 +0200

    Update registry.py

--- rent_platform/core/registry.py
+++ rent_platform/core/registry.py
@@ -1,1 +1,13 @@
-#
+# rent_platform/core/registry.py
+
+from typing import Callable
+
+MODULES: dict[str, Callable] = {}
+
+
+def register_module(name: str, router_factory: Callable):
+    MODULES[name] = router_factory
+
+
+def get_module(name: str):
+    return MODULES.get(name)
