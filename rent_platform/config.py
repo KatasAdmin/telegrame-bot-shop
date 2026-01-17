6071e36fcc34be2ca0d51b2196862be76de00be7
KatasAdmin <katasonoviktor@gmail.com>
2026-01-17 21:25:07 +0200

    Update config.py

--- rent_platform/config.py
+++ rent_platform/config.py
@@ -1,1 +1,21 @@
-#
+# rent_platform/config.py
+
+import os
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    # PLATFORM
+    PLATFORM_BOT_TOKEN: str | None = os.getenv("PLATFORM_BOT_TOKEN")
+
+    # TELEGRAM
+    TELEGRAM_API_URL: str = "https://api.telegram.org"
+
+    # ENV
+    ENV: str = os.getenv("ENV", "dev")
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
