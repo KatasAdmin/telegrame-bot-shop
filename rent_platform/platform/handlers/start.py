b7ff663163aa347670dc27eeccc662dcd7f6a0d1
KatasAdmin <katasonoviktor@gmail.com>
2026-01-21 14:04:25 +0200

    Update start.py

--- rent_platform/platform/handlers/start.py
+++ rent_platform/platform/handlers/start.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from aiogram.exceptions import SkipHandler
+from aiogram.dispatcher.event.handler import SkipHandler
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.filters import Command
