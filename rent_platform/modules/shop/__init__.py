7cc68feb145b63d1374427413934995376f51e28
KatasAdmin <katasonoviktor@gmail.com>
2026-01-24 21:56:11 +0200

    Update __init__.py

--- rent_platform/modules/shop/__init__.py
+++ rent_platform/modules/shop/__init__.py
@@ -1,1 +1,6 @@
-#
+from __future__ import annotations
+
+from rent_platform.core.registry import register_module
+from rent_platform.modules.shop.router import handle_update
+
+register_module("shop", handle_update)
