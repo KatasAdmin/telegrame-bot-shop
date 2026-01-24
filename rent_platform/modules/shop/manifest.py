ad4228ba97d3d5595e17839502630d6847fca942
KatasAdmin <katasonoviktor@gmail.com>
2026-01-24 23:30:17 +0200

    Delete manifest.py

--- rent_platform/modules/shop/manifest.py
+++
@@ -1,7 +0,0 @@
-MANIFEST = {
-    "name": "shop",
-    "title": "🛒 Магазин",
-    "description": "Повноцінний Telegram-магазин з товарами та замовленнями",
-    "version": "1.0.0",
-    "entry": "rent_platform.modules.shop.router:handle_update",
-}
