ca0174d77e49f75d8771cbd11596b27c4f4f5930
KatasAdmin <katasonoviktor@gmail.com>
2026-01-25 20:22:11 +0200

    Update 2026_01_27_merge_tg_categories_and_catid.py

--- alembic/versions/2026_01_27_merge_tg_categories_and_catid.py
+++ alembic/versions/2026_01_27_merge_tg_categories_and_catid.py
@@ -4,12 +4,9 @@ Revision ID: merge_tg_cats_catid_0127m
 Revises: tg_shop_categories_0127, tg_shop_products_catid_0127
 Create Date: 2026-01-27
 
-Це merge-міграція: зливає 2 head-и в один.
-Нічого в БД не робить.
+Це merge-міграція, нічого в БД не робить.
 """
 from __future__ import annotations
-
-from alembic import op
 
 revision = "merge_tg_cats_catid_0127m"
 down_revision = ("tg_shop_categories_0127", "tg_shop_products_catid_0127")
@@ -18,10 +15,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    # no-op (merge point)
     pass
 
 
 def downgrade() -> None:
-    # no-op (merge point)
     pass
